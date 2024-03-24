8665be8ce80179046cee8672552e3ed8c86b0dd0
gaboliveiradev <g38789138@gmail.com>
2024-03-24 08:17:49 -0300

    feat: modal add value plan

--- src/components/modals/AddValuePlanModal.js
+++ src/components/modals/AddValuePlanModal.js
@@ -14,7 +14,7 @@ export default function AddValuePlanModal() {
     return (
         <div class="min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 flex justify-center items-center inset-0 z-1 outline-none focus:outline-none" id="modal-id">
             <div class="absolute bg-black opacity-80 inset-0 z-2"></div>
-            <div class="w-[18%] max-w-2xl relative mx-auto my-auto shadow-lg ">
+            <div class="w-full max-w-2xl relative mx-auto my-auto shadow-lg ">
                 <div className='bg-green-table-items p-4 flex flex-row justify-between items-center'>
                     <h1 className='text-white font-bold text-[13px] uppercase'>Adicionando Valores</h1>
                     <div onClick={() => setAddValuePlanModal(false)} className='cursor-pointer text-white opacity-[0.7] hover:opacity-[1] font-bold'>

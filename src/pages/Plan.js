d73ae6b0591c575a8ea1138ab76e24e9c031d916
gaboliveiradev <g38789138@gmail.com>
2024-04-08 22:32:59 -0300

    refactor

--- src/pages/Plan.js
+++ src/pages/Plan.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import PlanDesktop from '../components/Pages/List/PlanDesktop';
-import { usePlanContext } from '../context/Plan';
-import PlanModal from '../components/Pages/Insert/Modals/PlanModal';
 
 export default function Plan() {
-
-    const { isModalVisible } = usePlanContext();
-
     return (
         <>
-            { isModalVisible && <PlanModal /> }
             <main className="mx-auto max-w-7xl px-2">
                 <div className='py-2 mx-auto'>
                     <PlanDesktop />
